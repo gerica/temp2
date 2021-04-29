9efde2c8fc8ce4b62cadc81f3f7d2a95ec0a801e
Denis Viana <denisvcosta@gmail.com>
2021-04-29 19:12:59 -0300

    Adds graphql dependencies and initial configuration

--- lib/di/di.config.dart
+++ lib/di/di.config.dart
@@ -4,18 +4,19 @@
 // InjectableConfigGenerator
 // **************************************************************************
 
-import 'package:dio/dio.dart' as _i6;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i7;
-import 'package:fresh_dio/fresh_dio.dart' as _i3;
+import 'package:dio/dio.dart' as _i3;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i8;
 import 'package:get_it/get_it.dart' as _i1;
+import 'package:graphql/client.dart' as _i4;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:shared_preferences/shared_preferences.dart' as _i4;
+import 'package:shared_preferences/shared_preferences.dart' as _i5;
 
-import '../core/data/remote/dio_client.dart' as _i5;
-import 'modules/device_module.dart' as _i10;
-import 'modules/local_module.dart' as _i9;
+import '../core/data/helpers/secure_local_storage.dart' as _i6;
+import '../core/data/network/dio_client.dart' as _i7;
+import 'modules/device_module.dart' as _i11;
+import 'modules/local_module.dart' as _i10;
 import 'modules/remote_module.dart'
-    as _i8; // ignore_for_file: unnecessary_lambdas
+    as _i9; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// an extension to register the provided dependencies inside of [GetIt]
@@ -28,22 +29,30 @@ extension GetItInjectableX on _i1.GetIt {
     final localModule = _$LocalModule();
     final deviceModule = _$DeviceModule();
     gh.factory<_i3.Dio>(() => remoteModule.provideDio());
-    await gh.factoryAsync<_i4.SharedPreferences>(() => localModule.prefs,
+    gh.factory<_i4.HttpLink>(() => remoteModule.provideHttpLink());
+    await gh.factoryAsync<_i5.SharedPreferences>(() => localModule.prefs,
         preResolve: true);
     gh.factory<String>(() => deviceModule.language, instanceName: 'language');
-    gh.factory<_i5.DioClient>(() => remoteModule.provideApi(
-        get<_i3.Dio>(), get<_i6.InterceptorsWrapper>()));
-    gh.singleton<_i7.FlutterSecureStorage>(localModule.storage);
-    gh.singleton<_i6.InterceptorsWrapper>(remoteModule.provideInterceptor(
+    gh.factory<_i4.WebSocketLink>(() => remoteModule.provideWebSocketLink());
+    await gh.factoryAsync<_i4.GraphQLClient>(
+        () => remoteModule.provideGraphQLClient(get<_i4.HttpLink>(),
+            get<_i4.WebSocketLink>(), get<_i6.SecureLocalStorage>()),
+        preResolve: true);
+    gh.factory<_i7.DioClient>(() => remoteModule.provideApi(
+        get<_i3.Dio>(), get<_i3.InterceptorsWrapper>()));
+    gh.singleton<_i8.FlutterSecureStorage>(localModule.storage);
+    gh.singleton<_i6.SecureLocalStorage>(
+        _i6.SecureLocalStorage(get<_i8.FlutterSecureStorage>()));
+    gh.singleton<_i3.InterceptorsWrapper>(remoteModule.provideInterceptor(
         get<_i3.Dio>(),
         get<String>(instanceName: 'language'),
-        get<_i7.FlutterSecureStorage>()));
+        get<_i8.FlutterSecureStorage>()));
     return this;
   }
 }
 
-class _$RemoteModule extends _i8.RemoteModule {}
+class _$RemoteModule extends _i9.RemoteModule {}
 
-class _$LocalModule extends _i9.LocalModule {}
+class _$LocalModule extends _i10.LocalModule {}
 
-class _$DeviceModule extends _i10.DeviceModule {}
+class _$DeviceModule extends _i11.DeviceModule {}
--- lib/di/modules/remote_module.dart
+++ lib/di/modules/remote_module.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:fresh_dio/fresh_dio.dart';
+import 'package:graphql/client.dart';
 import 'package:injectable/injectable.dart';
+import 'package:radio_life/core/data/helpers/secure_local_storage.dart';
 import 'package:radio_life/core/data/interceptors/radio_life_interceptor.dart';
-import 'package:radio_life/core/data/remote/dio_client.dart';
+import 'package:radio_life/core/data/network/dio_client.dart';
+import 'package:radio_life/core/data/network/radio_life_graphql_client.dart';
 import 'package:radio_life/flavors/flavor_values.dart';
 
 // Use this module to inject your third-party dependencies like [FirebaseAuth]
@@ -11,7 +13,6 @@ import 'package:radio_life/flavors/flavor_values.dart';
 
 @module
 abstract class RemoteModule {
-
   Dio provideDio() {
     final dio = Dio();
     dio.options.connectTimeout = 10000;
@@ -28,14 +29,25 @@ abstract class RemoteModule {
     return dio;
   }
 
+  HttpLink provideHttpLink() => HttpLink(FlavorConfig.instance.values.baseUrl);
+
+  WebSocketLink provideWebSocketLink() => WebSocketLink(
+        FlavorConfig.instance.values.baseWebSocketUrl,
+        config: const SocketClientConfig(autoReconnect: true),
+      );
+
+  @preResolve
+  Future<GraphQLClient> provideGraphQLClient(
+          HttpLink httpLink, WebSocketLink webSocketLink, SecureLocalStorage secureStorage) =>
+      RadioLifeGraphQLClient.init(httpLink, webSocketLink, secureStorage);
+
   @singleton
   InterceptorsWrapper provideInterceptor(
-      Dio dio, @Named('language') String language, FlutterSecureStorage flutterSecureStorage) =>
+          Dio dio, @Named('language') String language, FlutterSecureStorage flutterSecureStorage) =>
       RadioLifeInterceptor.init(dio, language, flutterSecureStorage);
 
   DioClient provideApi(Dio dio, InterceptorsWrapper interceptor) {
     dio.interceptors.add(interceptor);
     return DioClient(dio);
   }
-
 }
