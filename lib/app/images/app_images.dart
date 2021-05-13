8938597ac0abcbfe60ba5fafb942290f8aff36cf
Denis Viana <denisvcosta@gmail.com>
2021-05-13 10:01:27 -0300

    Adds and configures image files

---
+++ lib/app/images/app_images.dart
@@ -0,0 +1,11 @@
+abstract class AppImages {
+  static const logoHorizontalColor = 'assets/images/logo_horizontal_color.png';
+  static const logoVerticalColor = 'assets/images/logo_vertical_color.png';
+  static const icon = 'assets/images/icon.png';
+  static const blackMonoChromeLogoHorizontal = 'assets/images/black_monochrome_logo_horizontal.png';
+  static const blackMonoChromeLogoVertical = 'assets/images/black_monochrome_logo_vertical.png';
+  static const whiteMonoChromeLogoHorizontal = 'assets/images/white_monochrome_logo_horizontal.png';
+  static const whiteMonoChromeLogoVertical = 'assets/images/white_monochrome_logo_vertical.png';
+  static const grayMonoChromeLogoHorizontal = 'assets/images/gray_monochrome_logo_horizontal.png';
+  static const grayMonoChromeLogoVertical = 'assets/images/gray_monochrome_logo_vertical.png';
+}
