83bafeffbbf50ea2599031143c2fbdf0de5dbafe
Denis Viana <denisvcosta@gmail.com>
2021-06-21 18:24:35 -0300

    add bottom navigation bar; ui adjustments

--- lib/app/widget/navigation/app_bottom_navigation_bar.dart
+++ lib/app/widget/navigation/app_bottom_navigation_bar.dart
@@ -9,60 +9,56 @@ import '../../../generated/l10n.dart';
 import 'app_bottom_navitation_bar_controller.dart';
 
 class AppBottomNavigationBarWidget extends GetWidget<AppBottomNavigationController> {
-
   final int currentIndex;
 
   const AppBottomNavigationBarWidget({required this.currentIndex});
 
   @override
-  Widget build(BuildContext context) =>
-      GetBuilder<AppBottomNavigationController>(
+  Widget build(BuildContext context) => GetBuilder<AppBottomNavigationController>(
         initState: (_) {
           controller.changePage(currentIndex);
         },
-        builder: (logic) =>
-            Container(
-              color: AppColorScheme.white,
-              child: Obx(
-                    () =>
-                    BottomNavigationBar(
-                        items: [
-                          buildBottomNavigationBarItem(
-                            AppSvgImages.icProducts,
-                            S.of(context).products,
-                          ),
-                          buildBottomNavigationBarItem(
-                            AppSvgImages.icMyDevices,
-                            S.of(context).myDevices,
-                          ),
-                          buildBottomNavigationBarItem(
-                              AppSvgImages.icReports,
-                              S.of(context).reports
-                          ),
-                          buildBottomNavigationBarItem(
-                            AppSvgImages.icSupport,
-                            S.of(context).support,
-                          ),
-                          buildBottomNavigationBarItem(
-                            AppSvgImages.icProfile,
-                            S.of(context).profile,
-                          ),
-                        ],
-                        currentIndex: controller.bottomBarCurrentIndex.value,
-                        selectedFontSize: 10,
-                        unselectedFontSize: 10,
-                        backgroundColor: AppColorScheme.white,
-                        selectedItemColor: Colors.black,
-                        unselectedItemColor: const Color(0xFFA8A8A8),
-                        type: BottomNavigationBarType.fixed,
-                        onTap: _changeBottomBarNavigationIndex),
-              ),
-            ),
+        builder: (logic) => Container(
+          color: AppColorScheme.white,
+          child: Obx(
+            () => BottomNavigationBar(
+                items: [
+                  buildBottomNavigationBarItem(
+                    AppSvgImages.icProducts,
+                    S.of(context).products,
+                  ),
+                  buildBottomNavigationBarItem(
+                    AppSvgImages.icMyDevices,
+                    S.of(context).myDevices,
+                  ),
+                  buildBottomNavigationBarItem(AppSvgImages.icReports, S.of(context).reports),
+                  buildBottomNavigationBarItem(
+                    AppSvgImages.icSupport,
+                    S.of(context).support,
+                  ),
+                  buildBottomNavigationBarItem(
+                    AppSvgImages.icProfile,
+                    S.of(context).profile,
+                  ),
+                ],
+                currentIndex: controller.bottomBarCurrentIndex.value,
+                selectedFontSize: 10,
+                unselectedFontSize: 10,
+                backgroundColor: AppColorScheme.white,
+                selectedItemColor: Colors.black,
+                unselectedItemColor: const Color(0xFFA8A8A8),
+                type: BottomNavigationBarType.fixed,
+                onTap: _changeBottomBarNavigationIndex),
+          ),
+        ),
       );
 
   BottomNavigationBarItem buildBottomNavigationBarItem(String assetSvgImages, String label) =>
       BottomNavigationBarItem(
-        icon: PlatformSvg.asset(assetSvgImages, height: 25, fit: BoxFit.cover),
+        icon: Opacity(
+          opacity: 0.4,
+          child: PlatformSvg.asset(assetSvgImages, height: 25, fit: BoxFit.cover),
+        ),
         activeIcon: PlatformSvg.asset(assetSvgImages, height: 25),
         label: label,
       );
