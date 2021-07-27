75bfcc5f8e990fb623c3c0f76bb7b7b66da93c9f
Denis Viana <denisvcosta@gmail.com>
2021-07-26 21:14:14 -0300

    Change password

---
+++ lib/app/pages/sign_in/pages/create_password/create_password_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CreatePasswordPage extends GetView<CreatePasswordController> {
+
+  @override
+  Widget build(BuildContext context) => Container();
+  
+}
