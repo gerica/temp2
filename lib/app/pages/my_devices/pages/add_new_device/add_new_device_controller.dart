b50c240a832546fd2b40d11189fbe2a2718a7e1c
gerica <rogeriogerica@gmail.com>
2021-10-16 21:03:41 -0300

    #Sof-I118 write qrcode

--- lib/app/pages/my_devices/pages/add_new_device/add_new_device_controller.dart
+++ lib/app/pages/my_devices/pages/add_new_device/add_new_device_controller.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:radio_life/app/helper/validation_helper.dart';
+
+import '../../../../radio_life_app_routes.dart';
 
 class AddNewDeviceController extends GetxController {
-  //region Private
-  //endregion
-
-  //region Variables
   TextEditingController serialNumberController = TextEditingController();
+  final FocusNode serialNumberFocus = FocusNode();
+  final serialNumberError = ''.obs;
   TextEditingController locationController = TextEditingController();
+  final FocusNode locationFocus = FocusNode();
+  final locationError = ''.obs;
 
-  //endregion
+  void pressContinue() {
+    if (!_isValid) return;
+    Get.toNamed(Routes.autoScanPage);
+  }
 
-  //region Functions
-
-  //endregion
-
+  bool get _isValid {
+    bool result = true;
+    serialNumberError('');
+    locationError('');
+    final tempSN = ValidationHelper.validateField(serialNumberController.text);
+    if (tempSN != null) {
+      serialNumberError(tempSN);
+      result = false;
+    }
+    final tempLocation = ValidationHelper.validateField(locationController.text);
+    if (tempLocation != null) {
+      locationError(tempLocation);
+      result = false;
+    }
+    return result;
+  }
 }
--- lib/app/pages/my_devices/pages/add_new_device/add_new_device_page.dart
+++ lib/app/pages/my_devices/pages/add_new_device/add_new_device_page.dart
@@ -15,81 +15,104 @@ import 'add_new_device_controller.dart';
 
 class AddNewDevicePage extends GetView<AddNewDeviceController> {
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: RadioLifeAppBarWidget(
-          showBackButton: true,
-          brightness: Brightness.dark,
-          titleText: S.of(context).newDevice,
-          backgroundColor: AppColorScheme.primarySwatch,
-          onBackButtonPressed: () {
-            Get.back();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: RadioLifeAppBarWidget(
+        showBackButton: true,
+        brightness: Brightness.dark,
+        titleText: S.of(context).newDevice,
+        backgroundColor: AppColorScheme.primarySwatch,
+        onBackButtonPressed: () {
+          Get.back();
+        },
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(AppSpacing.medium),
+        child: Obx(() => SingleChildScrollView(
+              child: Column(
+                children: [
+                  _buildForms(context),
+                  UIHelper.verticalSpaceLarge,
+                  PrimaryButton(
+                    onPressed: () => controller.pressContinue(),
+                    title: S.of(context).continue_label,
+                    color: PrimaryButtonColor.primary,
+                    type: PrimaryButtonType.circular,
+                    style: PrimaryButtonStyle.filled,
+                    state: Status.success,
+                  ),
+                  UIHelper.verticalSpaceMedium,
+                  PrimaryButton(
+                    onPressed: () async {
+                      final result = await Get.toNamed(Routes.qrCodeViewer);
+                      if (result != null) {
+                        final initSerialNumber = result.indexOf('sr:');
+                        if (initSerialNumber >= 0) {
+                          controller.serialNumberController.text = result.substring(initSerialNumber + 4, initSerialNumber + 9);
+                          controller.serialNumberError('');
+                        }
+                        final initLocation = result.indexOf('lt:');
+                        if (initLocation >= 0) {
+                          controller.locationController.text = result.substring(initLocation + 4, result.length);
+                          controller.locationError('');
+                        }
+                      }
+                    },
+                    title: S.of(context).qrCode,
+                    color: PrimaryButtonColor.primary,
+                    type: PrimaryButtonType.circular,
+                    style: PrimaryButtonStyle.bordered,
+                    state: Status.success,
+                  ),
+                ],
+              ),
+            )),
+      ),
+      bottomNavigationBar: const AppBottomNavigationBarWidget(
+        currentIndex: 0,
+      ),
+    );
+  }
+
+  Widget _buildForms(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        InputTextWidget(
+          hintText: S.of(context).serialNumber,
+          keyboardType: TextInputType.number,
+          textCapitalization: TextCapitalization.words,
+          controller: controller.serialNumberController,
+          errorText: controller.serialNumberError.value == '' ? null : controller.serialNumberError.value,
+          focusNode: controller.serialNumberFocus,
+          onFieldSubmitted: () {
+            _fieldFocusChange(context, controller.serialNumberFocus, controller.locationFocus);
+            if (controller.serialNumberError.value.isNotEmpty) {
+              controller.serialNumberError('');
+            }
+          },
+          textInputAction: TextInputAction.next,
+        ),
+        UIHelper.verticalSpaceMedium,
+        InputTextWidget(
+          hintText: S.of(context).location,
+          keyboardType: TextInputType.text,
+          textCapitalization: TextCapitalization.words,
+          controller: controller.locationController,
+          errorText: controller.locationError.value == '' ? null : controller.locationError.value,
+          focusNode: controller.locationFocus,
+          onFieldSubmitted: () {
+            if (controller.locationError.value.isNotEmpty) {
+              controller.locationError('');
+            }
           },
         ),
-        body: Container(
-          padding: const EdgeInsets.all(AppSpacing.medium),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                _buildForms(context),
-                UIHelper.verticalSpaceLarge,
-                PrimaryButton(
-                  onPressed: () {
-                    Get.toNamed(Routes.autoScanPage);
-                  },
-                  title: S.of(context).continue_label,
-                  color: PrimaryButtonColor.primary,
-                  type: PrimaryButtonType.circular,
-                  style: PrimaryButtonStyle.filled,
-                  state: Status.success,
-                ),
-                UIHelper.verticalSpaceMedium,
-                PrimaryButton(
-                  onPressed: () async {
-                    final result = await Get.toNamed(Routes.qrCodeViewer);
-                    if (result != null)
-                      Get.snackbar(
-                        S.of(context).qrCode,
-                        result,
-                        snackPosition: SnackPosition.BOTTOM,
-                        colorText: Colors.black,
-                        margin: const EdgeInsets.all(16),
-                      );
-                  },
-                  title: S.of(context).qrCode,
-                  color: PrimaryButtonColor.primary,
-                  type: PrimaryButtonType.circular,
-                  style: PrimaryButtonStyle.bordered,
-                  state: Status.success,
-                ),
-              ],
-            ),
-          ),
-        ),
-        bottomNavigationBar: const AppBottomNavigationBarWidget(
-          currentIndex: 0,
-        ),
-      );
+      ],
+    );
+  }
 
-  Widget _buildForms(BuildContext context) => Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          InputTextWidget(
-            hintText: S.of(context).serialNumber,
-            onFieldSubmitted: () {},
-            keyboardType: TextInputType.number,
-            textCapitalization: TextCapitalization.words,
-            controller: controller.serialNumberController,
-            errorText: null,
-          ),
-          UIHelper.verticalSpaceMedium,
-          InputTextWidget(
-            hintText: S.of(context).location,
-            onFieldSubmitted: () {},
-            keyboardType: TextInputType.text,
-            textCapitalization: TextCapitalization.words,
-            controller: controller.locationController,
-            errorText: null,
-          ),
-        ],
-      );
+  void _fieldFocusChange(BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
 }
